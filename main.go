b22e24fe8d9c3ff9f856c0a8c168a1c6ef484d2c
Sanyia Saidova <sanyia.saidova@gmail.com>
2022-01-25 20:41:10 +0000

    Changing discord client API

--- main.go
+++ main.go
@@ -1,74 +1,89 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+	"context"
+	"log"
 	"os"
-	"os/signal"
-	"syscall"
 
-	"github.com/bwmarrin/discordgo"
+	"github.com/diamondburned/arikawa/v3/api"
+	"github.com/diamondburned/arikawa/v3/discord"
+	"github.com/diamondburned/arikawa/v3/gateway"
+	"github.com/diamondburned/arikawa/v3/state"
+	"github.com/diamondburned/arikawa/v3/utils/json/option"
 )
 
-// Variables used for command line parameters
-var (
-	Token string
-)
-
-func init() {
-
-	flag.StringVar(&Token, "t", "", "Bot Token")
-	flag.Parse()
-}
+// To run, do `GUILD_ID="GUILD ID" BOT_TOKEN="TOKEN HERE" go run .`
 
 func main() {
+	guildID := discord.GuildID(mustSnowflakeEnv("GUILD_ID"))
 
-	// Create a new Discord session using the provided bot token.
-	dg, err := discordgo.New("Bot " + Token)
-	if err != nil {
-		fmt.Println("error creating Discord session,", err)
-		return
+	token := os.Getenv("BOT_TOKEN")
+	if token == "" {
+		log.Fatalln("No $BOT_TOKEN given.")
 	}
 
-	// Register the messageCreate func as a callback for MessageCreate events.
-	dg.AddHandler(messageCreate)
+	s := state.New("Bot " + token)
 
-	// In this example, we only care about receiving message events.
-	dg.Identify.Intents = discordgo.IntentsGuildMessages
-
-	// Open a websocket connection to Discord and begin listening.
-	err = dg.Open()
+	app, err := s.CurrentApplication()
 	if err != nil {
-		fmt.Println("error opening connection,", err)
-		return
+		log.Fatalln("Failed to get application ID:", err)
 	}
 
-	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-	<-sc
+	s.AddHandler(func(e *gateway.InteractionCreateEvent) {
+		data := api.InteractionResponse{
+			Type: api.MessageInteractionWithSource,
+			Data: &api.InteractionResponseData{
+				Content: option.NewNullableString("Pong!"),
+			},
+		}
 
-	// Cleanly close down the Discord session.
-	dg.Close()
+		if err := s.RespondInteraction(e.ID, e.Token, data); err != nil {
+			log.Println("failed to send interaction callback:", err)
+		}
+	})
+
+	s.AddIntents(gateway.IntentGuilds)
+	s.AddIntents(gateway.IntentGuildMessages)
+
+	if err := s.Open(context.Background()); err != nil {
+		log.Fatalln("failed to open:", err)
+	}
+	defer s.Close()
+
+	log.Println("Gateway connected. Getting all guild commands.")
+
+	commands, err := s.GuildCommands(app.ID, guildID)
+	if err != nil {
+		log.Fatalln("failed to get guild commands:", err)
+	}
+
+	for _, command := range commands {
+		log.Println("Existing command", command.Name, "found. Deleting..")
+		s.DeleteGuildCommand(app.ID, guildID, command.ID)
+	}
+
+	newCommands := []api.CreateCommandData{
+		{
+			Name:        "rng-party",
+			Description: "Create a new randomly generated party",
+		},
+	}
+
+	for _, command := range newCommands {
+		_, err := s.CreateGuildCommand(app.ID, guildID, command)
+		if err != nil {
+			log.Fatalln("failed to create guild command:", err)
+		}
+	}
+
+	// Block forever.
+	select {}
 }
 
-// This function will be called (due to AddHandler above) every time a new
-// message is created on any channel that the authenticated bot has access to.
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
-	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
-	if m.Author.ID == s.State.User.ID {
-		return
+func mustSnowflakeEnv(env string) discord.Snowflake {
+	s, err := discord.ParseSnowflake(os.Getenv(env))
+	if err != nil {
+		log.Fatalf("Invalid snowflake for $%s: %v", env, err)
 	}
-	// If the message is "ping" reply with "Pong!"
-	if m.Content == "ping" {
-		s.ChannelMessageSend(m.ChannelID, "Pong!")
-	}
-
-	// If the message is "pong" reply with "Ping!"
-	if m.Content == "pong" {
-		s.ChannelMessageSend(m.ChannelID, "Ping!")
-	}
+	return s
 }
